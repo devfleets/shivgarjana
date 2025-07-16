cda0b08fcad99bdf4f5efd7bcc77f8973825eadd
DevFleets <devfleets@gmail.com>
2025-07-16 23:57:19 +0530

    imagechange

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ export default function Card() {
     return (
         <>
             <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <img className="p-8 rounded-t-lg" src="/shivgarjanapathakpunelogo.png" alt="product image" />
-                </a>
+               <a href="#">
+                    <img className="p-8 rounded-t-lg" src="/public/shivgarjanapathakpunelogo.png" alt="product image" />
+                </a> 
                 <div className="px-5 pb-5">
                     <a href="#">
                         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">शिवगर्जना ढोल ताशा ध्वज पथक | पुणे</h5>
